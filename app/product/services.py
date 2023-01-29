685fcf51ef8689396419202777483143bde6ff7e
Cody McNeill <codymcneill@live.com>
2023-01-29 14:35:55 -0700

    basic product display on product page

--- app/product/services.py
+++ app/product/services.py
@@ -5,5 +5,13 @@ def getProducts():
     response = requests.get(url)
     if response.ok:
             data = response.json()
-            print(data)
+            for i in data:
+                product = {}
+                product['id'] = i['id']
+                product['title'] = i['title']
+                product['price'] = i['price']
+                product['description'] = i['description']
+                product['image'] = i['image']
+                print(product)
+            return data
 getProducts()
