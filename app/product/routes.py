07bca85e45bb1bf4ddccc734aed7e4ed62f70deb
Cody McNeill <codymcneill@live.com>
2023-01-30 16:02:03 -0700

    more productPage work

--- app/product/routes.py
+++ app/product/routes.py
@@ -1,6 +1,6 @@
 from app import app
 from flask import render_template, request, redirect, url_for, Blueprint
-from .services import getProducts
+from .services import getProducts, getProductID
 
 product = Blueprint('product', __name__)
 
@@ -8,4 +8,10 @@ product = Blueprint('product', __name__)
 @product.route('/products')
 def productsPage():
     content = getProducts()
-    return render_template('products.html', content=content)
+    prod_id = getProductID()
+    return render_template('products.html', content=content, prod_id=prod_id)
+
+@product.route('/products/<prod_id>')
+def getProduct(prod_id):
+    prod_id = getProductID()
+    return render_template('singleproduct.html', prod_id = prod_id)
